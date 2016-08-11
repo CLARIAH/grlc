40e49d1a2c9819859e0273b5856c8d39f4498a64
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2016-08-11 15:41:54 +0200

    Adding docker image

---
+++ docker-build/assets/build/install.sh
@@ -0,0 +1,63 @@
+#!/bin/bash
+set -e
+GRLC_CLONE_URL=https://github.com/CLARIAH/grlc.git
+
+## Execute a command as GITLAB_USER
+exec_as_grlc() {
+  if [[ $(whoami) == ${GRLC_USER} ]]; then
+    $@
+  else
+    sudo -HEu ${GRLC_USER} "$@"
+  fi
+}
+
+#add grlc user
+adduser --disabled-login --gecos 'Grlc' ${GRLC_USER}
+passwd -d ${GRLC_USER}
+
+#configure git
+exec_as_grlc git config --global core.autocrlf input
+exec_as_grlc git config --global gc.auto 0
+
+
+
+exec_as_grlc git clone --recursive ${GRLC_CLONE_URL} ${GRLC_INSTALL_DIR}
+cd ${GRLC_INSTALL_DIR}
+pip install -r requirements.txt
+
+
+
+
+
+#move nginx logs to ${GITLAB_LOG_DIR}/nginx
+sed -i \
+ -e "s|access_log /var/log/nginx/access.log;|access_log ${GRLC_LOG_DIR}/nginx/access.log;|" \
+ -e "s|error_log /var/log/nginx/error.log;|error_log ${GRLC_LOG_DIR}/nginx/error.log;|" \
+ /etc/nginx/nginx.conf
+
+
+ # configure gitlab log rotation
+ cat > /etc/logrotate.d/grlc << EOF
+ ${GRLC_LOG_DIR}/grlc/*.log {
+   weekly
+   missingok
+   rotate 52
+   compress
+   delaycompress
+   notifempty
+   copytruncate
+ }
+ EOF
+
+ # configure gitlab vhost log rotation
+ cat > /etc/logrotate.d/grlc-nginx << EOF
+ ${GRLC_LOG_DIR}/nginx/*.log {
+   weekly
+   missingok
+   rotate 52
+   compress
+   delaycompress
+   notifempty
+   copytruncate
+ }
+ EOF
---
+++ docker-build/entrypoint.sh
@@ -0,0 +1,49 @@
+#!/bin/bash
+set -e
+source ${GRLC_RUNTIME_DIR}/functions
+
+[[ $DEBUG == true ]] && set -x
+
+setup_nginx;
+
+case ${1} in
+  app:start)
+
+    # initialize_system
+    # configure_gitlab
+    # configure_gitlab_shell
+    # configure_nginx
+
+    case ${1} in
+      app:start)
+        cd ${GRLC_INSTALL_DIR}
+        python grlc.py
+        # migrate_database
+        # rm -rf /var/run/supervisor.sock
+        # exec /usr/bin/supervisord -nc /etc/supervisor/supervisord.conf
+        ;;
+      # app:init)
+      #   migrate_database
+      #   ;;
+      # app:sanitize)
+      #   sanitize_datadir
+      #   ;;
+      # app:rake)
+      #   shift 1
+      #   execute_raketask $@
+      #   ;;
+    esac
+    ;;
+  app:help)
+    echo "Available options:"
+    echo " app:start        - Starts the grlc server (default)"
+    # echo " app:init         - Initialize the gitlab server (e.g. create databases, compile assets), but don't start it."
+    # echo " app:sanitize     - Fix repository/builds directory permissions."
+    # echo " app:rake <task>  - Execute a rake task."
+    echo " app:help         - Displays the help"
+    echo " [command]        - Execute the specified command, eg. bash."
+    ;;
+  *)
+    exec "$@"
+    ;;
+esac
