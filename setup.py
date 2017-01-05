c3dd51595413a077e8c086c6c2814fb4f38f23df
Carlos Martinez <c.martinez@esciencecenter.nl>
2017-01-05 11:37:59 +0100

    Add package_data to setup.py

--- setup.py
+++ setup.py
@@ -1,9 +1,13 @@
 #!/usr/bin/env python
 
 from distutils.core import setup
+import os
 
 with open('requirements.txt') as f:
     required = f.read().splitlines()
+
+grlc_base = 'src/'
+grlc_data = [ root.replace(grlc_base, '') + '/*' for root,dirs,files in os.walk(grlc_base) ]
 
 setup(name='grlc',
       version='1.0',
@@ -12,6 +16,7 @@ setup(name='grlc',
       url='https://github.com/CLARIAH/grlc',
       packages=['grlc'],
       package_dir = {'grlc': 'src'},
+      package_data = {'grlc': grlc_data},
       scripts=['bin/grlc-server'],
       install_requires=required
      )
