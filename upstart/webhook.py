f47730733b9740ecb5b26fc74ec15b2542344a58
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2018-09-11 13:06:08 +0200

    working hook

--- upstart/webhook.py
+++ upstart/webhook.py
@@ -5,14 +5,14 @@ from subprocess import call
 import os
 app = Flask(__name__)
 
-@app.route("/", method=['POST'])
+@app.route("/", methods=['POST'])
 def update():
     print "Starting image update"
     call(['docker', 'pull', 'clariah/grlc:dev'])
-    os.chdir('/home/amp')
-    call(['docker-compose', '-f /home/amp/src/grlc-dev/docker-compose.default.yml', 'restart'])
+    call(['docker-compose', '-f', '/home/amp/src/grlc-dev/docker-compose.default.yml', 'restart'])
     print "All done; exiting..."
 
+    return 200
+
 if __name__ == '__main__':
-    update()
-    exit(0)
+    app.run(host='0.0.0.0', port=8004, debug=True)
