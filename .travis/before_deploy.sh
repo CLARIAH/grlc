b8dc0b1f8dea8d4139353efb6f2080eeeffc62c6
Carlos Martinez <c.martinez@esciencecenter.nl>
2018-10-12 16:13:03 +0200

    Test and deploy jobs

--- .travis/before_deploy.sh
+++
@@ -1,7 +0,0 @@
-#!/bin/bash
-
-# Deactivate testing virtualenv and reactivate original one
-deactivate
-echo "Reactivating virtualenv:"
-echo $ORIG_ENV
-source $ORIG_ENV
--- .travis/before_install.sh
+++ .travis/before_install.sh
@@ -1,10 +1,5 @@
 #!/bin/bash
 
-# Store location of original virtualenv so we can reactivate it later
-ORIG_ENV="$(dirname $(which python))/activate"
-echo "Original virtualenv"
-echo $ORIG_ENV
-
 if [[ $TRAVIS_OS_NAME == 'osx' ]]; then
   brew install pyenv
   pyenv install -s $PYENV_VERSION
