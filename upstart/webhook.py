6d5085f5927bcc30a851a8b99b38acb899c95c61
Pasquale Lisena <pasquale.lisena@eurecom.fr>
2019-02-14 18:26:24 +0100

    update to py3

--- upstart/webhook.py
+++ upstart/webhook.py
@@ -2,17 +2,19 @@
 
 from flask import Flask
 from subprocess import call
-import os
+
 app = Flask(__name__)
+
 
 @app.route("/", methods=['POST'])
 def update():
-    print "Starting image update"
+    print("Starting image update")
     call(['docker', 'pull', 'clariah/grlc:dev'])
     call(['docker-compose', '-f', '/home/amp/src/grlc-dev/docker-compose.default.yml', 'restart'])
-    print "All done; exiting..."
+    print("All done; exiting...")
 
     return 200
+
 
 if __name__ == '__main__':
     app.run(host='0.0.0.0', port=8004, debug=True)
