2513f6aad438dd629c4e0817d5ec958676baedb6
Albert Meroño-Peñuela <albert.meronyo@gmail.com>
2020-02-11 14:35:57 +0100

    specUrl param for executing callnames

--- src/swagger.py
+++ src/swagger.py
@@ -110,9 +110,9 @@ def get_path_for_item(item):
     return item_path
 
 
-def build_spec(user, repo, subdir=None, query_urls=None, sha=None, prov=None, extraMetadata=[]):
+def build_spec(user, repo, subdir=None, query_url=None, sha=None, prov=None, extraMetadata=[]):
     """Build grlc specification for the given github user / repo."""
-    loader = grlc.utils.getLoader(user, repo, subdir, query_urls, sha=sha, prov=prov)
+    loader = grlc.utils.getLoader(user, repo, subdir, query_url, sha=sha, prov=prov)
 
     files = loader.fetchFiles()
     raw_repo_uri = loader.getRawRepoUri()
@@ -122,10 +122,10 @@ def build_spec(user, repo, subdir=None, query_urls=None, sha=None, prov=None, ex
 
     allowed_ext = ["rq", "sparql", "json", "tpf"]
     for c in files:
-        glogger.debug(files)
+        # glogger.debug(files)
         glogger.debug('>>>>>>>>>>>>>>>>>>>>>>>>>c_name: {}'.format(c['name']))
         extension = c['name'].split('.')[-1]
-        if extension in allowed_ext or query_urls: # parameter provided queries may not have extension
+        if extension in allowed_ext or query_url: # parameter provided queries may not have extension
             call_name = c['name'].split('.')[0]
 
             # Retrieve extra metadata from the query decorators
@@ -135,7 +135,7 @@ def build_spec(user, repo, subdir=None, query_urls=None, sha=None, prov=None, ex
             if extension == "json":
                 query_text = json.loads(query_text)
 
-            if extension in ["rq", "sparql", "json"] or query_urls:
+            if extension in ["rq", "sparql", "json"] or query_url:
                 glogger.debug("===================================================================")
                 glogger.debug("Processing SPARQL query: {}".format(c['name']))
                 glogger.debug("===================================================================")
@@ -260,6 +260,16 @@ def process_sparql_query_text(query_text, loader, call_name, extraMetadata):
         endpoint_param['description'] = "Alternative endpoint for SPARQL query"
         endpoint_param['default'] = endpoint
         params.append(endpoint_param)
+
+    # If this is a URL generated spec we need to force API calls with the specUrl parameter set
+    if type(loader) is ParamLoader:
+        specUrl_param = {}
+        specUrl_param['name'] = "specUrl"
+        specUrl_param['type'] = "string"
+        specUrl_param['in'] = "query"
+        specUrl_param['description'] = "URL of the API specification"
+        specUrl_param['default'] = loader.getSpecUrl()
+        params.append(specUrl_param)
 
     if query_metadata['type'] == 'SelectQuery':
         # Fill in the spec for SELECT
