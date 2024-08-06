0d659eae1c7ddcd6f5562ae1a86baae9d96d1fe6
Carlos Martinez <neocarlitos@gmail.com>
2024-08-06 22:35:25 +0200

    Check JSON files for transformer queries

--- tests/test_swagger.py
+++ tests/test_swagger.py
@@ -29,7 +29,8 @@ class TestSwagger(unittest.TestCase):
         repo = "testrepo"
         spec, warnings = build_spec(user, repo, git_type="github")
 
-        self.assertEqual(len(spec), len(filesInRepo))
+        # Repo contains one JSON file which is not a query, and should be ignored
+        self.assertEqual(len(spec), len(filesInRepo) - 1)
 
 
 if __name__ == "__main__":
